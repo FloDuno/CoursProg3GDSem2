68cd0c1e9cc07ce4e1b3128a50ab2ccddf515b1f
Flore <florianmeyer67@gmail.com>
2017-05-04 22:28:52 +0200

    Add all work

---
+++ Cours/Assets/Scripts/Animate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+
+    /// <summary>
+    /// My Animator
+    /// </summary>
+    private Animator _animator;
+
+	// Use this for initialization
+    private void Start ()
+    {
+        _animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+    private void Update ()
+    {
+        float _forwardSpeed = Input.GetAxis("Vertical");
+        float _turnSpeed = Input.GetAxis("Horizontal");
+        //Pour que ça marche parfaitement, utilisez des courbes linéaires dans les animations
+        //Faites attention aux majuscules, espaces etc.
+        //La descélération est gérée directement par Unity
+        //Oubliez pas que les GetAxis vont de -1 à 1
+        _animator.SetFloat("Speed", _forwardSpeed);
+        _animator.SetFloat("Turn", _turnSpeed);
+
+    }
+}
---
+++ Cours/Assets/Scripts/ScreenShake.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour {
+
+    /// <summary>
+    /// Courbe d'animation à modifier dans l'inspector
+    /// </summary>
+    [SerializeField] private AnimationCurve _curve;
+
+    [SerializeField] private GameObject _prefab;
+
+	// Update is called once per frame
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LaunchAnim());
+            //	        Spawn();
+        }
+    }
+
+    /// <summary>
+    /// Utilisée pour montrer comment fonctionne le profiler
+    /// </summary>
+    private void Spawn()
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            Instantiate(_prefab);
+        }
+    }
+
+    /// <summary>
+    /// Coroutine gérant une animation selon le temps passé et une courbe d'animation
+    /// Ce qui est avant le while est exécuté au lancement, ce qui est après à la fin et dedans c'est à chaque frame
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator LaunchAnim()
+    {
+        //Temps écoulé depuis le lancement
+        float _actualTime = 0;
+        //Tant que le temps n'a pas dépassé le temps de la courbe
+        while (_actualTime < _curve.keys[_curve.length-1].time) //_curve.keys[_curve.length - 1] = le dernier point de la courbe. On récupère donc son timecode (valeur en X)
+        {
+            //On compte le temps passé en ajoutant le temps écoulé depuis la dernière frame
+            _actualTime += Time.deltaTime;
+            //On fait varier le y de la position selon la courbe
+            transform.position = new Vector3(0,_curve.Evaluate(_actualTime),0);//Evaluate prend en paramètre la valeur en x et renvoie la valeur en y de la courbe d'animation
+            //Permet de passer à la frame suivante
+            yield return null;
+        }
+        //Test pour montrer qu'on peux exécuter une instruction à la fin de la coroutine
+        print("tsreirsu");
+    }
+}
